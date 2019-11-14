3a1a7bd071e2fa4f5c347e7987516b9bcca2254e
4iner <foreigncode@gmail.com>
2019-11-13 21:31:33 -0500

    initial commit - maven project built - skeleton interfaces

---
+++ src/main/java/adapter/AudioFormatter.java
@@ -0,0 +1,11 @@
+package adapter;
+
+/**
+ * An interface to use some kind of audio formatter to convert audio to certain formats
+ * Some tools only accept certain formats, e.g. CMUSphinx only accepts audio files of 
+ * the format RIFF (little-endian) data, WAVE audio, Microsoft PCM, 16 bit, mono 16000 Hz
+ * 
+ */
+public interface AudioFormatter {
+    
+}
---
+++ src/main/java/adapter/STTTool.java
@@ -0,0 +1,9 @@
+package adapter;
+
+/**
+ * Interface for implementing a Speech-To-Text tool
+ */
+public interface STTTool {
+    
+    // TODO: Add methods to transcript text given an audio file
+}
